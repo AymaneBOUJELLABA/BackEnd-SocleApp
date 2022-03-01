96c0c9ff52c1e644a96f8d6bdb3be3bd89ddf10f
AymaneBOUJELLABA <aymane.boujellaba@gmail.com>
2022-03-01 21:29:06 +0100

    initial commit

---
+++ src/main/java/fr/ubo/socleapp/SocleappApplication.java
@@ -0,0 +1,12 @@
+package fr.ubo.socleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocleappApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/ubo/socleapp/SocleappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ubo.socleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
